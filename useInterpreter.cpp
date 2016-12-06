96e9b161dcd85b142886642e6fb61054e93a8cd7
Clinton Poserio <notnilc@users.noreply.github.com>
2016-12-06 16:10:59 +0800

    Encoded Program
    Program copied from Data Structures and Algorithms in C++, Drozdek

---
+++ useInterpreter.cpp
@@ -0,0 +1,14 @@
+#include "interpreter.h"
+
+using namespace std;
+
+int main(){
+	Statement statement;
+	cout << "The program processes statements of the following format:\n"
+			<< "\t<id> = <expr>;\n\tprint <id>\n\tstatus\n\tend\n\n";
+	while(true){
+		statement.getStatement();
+	}
+
+	return 0;
+}
